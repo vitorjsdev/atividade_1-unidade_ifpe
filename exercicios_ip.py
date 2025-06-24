33c4b7f8edf5f162f89c0f9b2edf9727b34b00dd
Vitor José <vjds@discente.ifpe.edu.br>
2025-06-24 18:25:33 -0300

    código completo

---
+++ exercicios_ip.py
@@ -0,0 +1,56 @@
+#Questão 1: 
+matriz = []
+
+linhas = int(input("número de linhas da matriz: "))
+colunas = int(input("número de colunas: "))
+
+for i in range(linhas):
+    linhas_matriz = []
+    for j in range(colunas):
+        if i < j:
+            linhas_matriz.append((2 * i + 7 * j + 2))
+        elif i == j:
+            linhas_matriz.append((3 * (i**2) + 1))
+        else:
+            linhas_matriz.append(4 * (i**3) + 5 * (j**2))
+    matriz.append(linhas_matriz) 
+
+
+print(f"\nMatriz {linhas} x {colunas}")
+for i in range(linhas):
+    for j in range(colunas):
+        print(matriz[i][j], end=" ")
+    print(" ")
+
+
+#Questão 2: 
+
+gabarito = []
+print("Qual o gabarito da prova? ")
+for i in range(5):
+    resposta_gabarito = input(f"Questão {i + 1}: ")
+    gabarito.append(resposta_gabarito)
+
+aprovados = 0
+for a in range(5):
+    aluno = a + 1
+    print(f"\nInforme as respostas do aluno {aluno}")
+    
+    respostas = []
+    for b in range(5):
+        resposta_aluno = input(f"Questão {b + 1}: ")
+        respostas.append(resposta_aluno)
+
+    questoes_certas = 0
+    for c in range(5):
+        if respostas[c] == gabarito[c]:
+            questoes_certas += 1
+    
+    nota_fim = questoes_certas * 2.0
+    
+    if nota_fim >= 7:
+        aprovados += 1
+
+porc_de_aprovados = (aprovados / 5) * 100
+
+print("O percentual de aprovação é de "(int(aprovados/5) * 100))
